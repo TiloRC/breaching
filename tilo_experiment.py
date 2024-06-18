779aef19a7f1598944730755b3d1b85bdf4f2c86
Tilo Reneau-Cardoso <tiloreneau@gmail.com>
2024-06-18 10:33:49 -0500

    save inprogress images (and more command line options)

--- tilo_experiment.py
+++ tilo_experiment.py
@@ -5,6 +5,7 @@ import torch
 import breaching
 import sys
 import argparse
+import os
 
 
 def configure_experiment(cfg, num_data_points, num_local_updates, num_data_per_local_update_step, optimizer,
@@ -23,7 +24,7 @@ def configure_experiment(cfg, num_data_points, num_local_updates, num_data_per_l
     return cfg
 
 
-def run_experiment(gpu_index, max_iterations, optimizer="SGD", optim_callback=1000,
+def run_experiment(gpu_index, max_iterations, name, optimizer="SGD", callback_interval=100,
                    num_data_points=2, num_local_updates=1, num_data_per_local_update_step=2,
                    seed=47):
     logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)], format='%(message)s')
@@ -37,7 +38,7 @@ def run_experiment(gpu_index, max_iterations, optimizer="SGD", optim_callback=10
         print("Using cpu")
     setup = dict(device=device, dtype=getattr(torch, cfg.case.impl.dtype))
     cfg = configure_experiment(cfg, num_data_points, num_local_updates, num_data_per_local_update_step, optimizer,
-                               optim_callback, max_iterations)
+                               callback_interval, max_iterations)
 
     # Construct components
     user, server, model, loss_fn = breaching.cases.construct_case(cfg.case, setup)
@@ -52,12 +53,19 @@ def run_experiment(gpu_index, max_iterations, optimizer="SGD", optim_callback=10
     # Reconstruct data
     results = []
     def calculate_metrics_callback(candidate, iteration, trial, labels):
+        # save metrics data
         reconstructed_data = dict(data=candidate, labels=None)
         metrics = breaching.analysis.report(reconstructed_data, true_user_data, [server_payload],
                                             server.model, order_batch=True, compute_full_iip=False,
                                             cfg_case=cfg.case, setup=setup, verbose=False)
         metrics['iteration'] = iteration + 1
         results.append(metrics)
+
+        # save reconstruction image
+        #if (iteration+1) % (callback_interval*2) == 0:
+        user.plot(reconstructed_data)
+        plt.savefig(name +"/inprogress/"+ name + "_iter"+str(iteration+1) + "_reconstruction.png")
+
     reconstructed_user_data, stats = attacker.reconstruct([server_payload], [shared_data], {}, dryrun=cfg.dryrun, callback=calculate_metrics_callback)
 
     return user, true_user_data, reconstructed_user_data, pd.DataFrame(results).set_index('iteration')
@@ -69,21 +77,35 @@ if __name__ == "__main__":
     parser.add_argument('gpu_index', type=int, help='GPU index to use.')
     parser.add_argument('max_iterations', type=int, help='Maximum number of iterations.')
     parser.add_argument('optimizer', type=str, help='Optimizer to use (e.g., SGD, Adam).')
-    parser.add_argument('output_csv', type=str, help='Output CSV file name.')
-
+    parser.add_argument('experiment_name', type=str, help='Output CSV file name.')
+    parser.add_argument('--callback_interval', type=int, help='Interval at which the callback function is called', default=100)
+    parser.add_argument('--batch_size', type=int, help='Number of images to use when calculating update',
+                        default=1)
     args = parser.parse_args()
+
+    # create folder to store experiment results
+    try:
+        os.makedirs(args.experiment_name)
+    except FileExistsError:
+        pass
+    os.makedirs(args.experiment_name + "/inprogress") # folder for in progress images
+
     user, true_user_data, reconstructed_user_data, results_df = run_experiment(
+        name= args.experiment_name,
         gpu_index=args.gpu_index,
         max_iterations=args.max_iterations,
-        optimizer=args.optimizer
+        optimizer=args.optimizer,
+        callback_interval=args.callback_interval,
+        num_data_points=args.batch_size, num_local_updates=1, num_data_per_local_update_step=args.batch_size
     )
 
+    folder = args.experiment_name + "/"
     user.plot(true_user_data)
-    plt.savefig(args.output_csv +'_ground_truth.png')
+    plt.savefig(folder + args.experiment_name +'_ground_truth.png')
     user.plot(reconstructed_user_data)
-    plt.savefig(args.output_csv + '_reconstruction.png')
+    plt.savefig(folder + args.experiment_name + '_reconstruction.png')
 
-    torch.save(true_user_data, args.output_csv + "_ground_truth.pt")
-    torch.save(reconstructed_user_data, args.output_csv + "_reconstruction.pt")
-    results_df.to_csv(args.output_csv + ".csv")
+    torch.save(true_user_data, folder + args.experiment_name + "_ground_truth.pt")
+    torch.save(reconstructed_user_data, folder + args.experiment_name + "_reconstruction.pt")
+    results_df.to_csv(folder + args.experiment_name + ".csv")
 
